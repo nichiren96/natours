167bddfa8ab6d99907fb5d99f54943e4e39627ef
Roel Tombozafy <rtombozafy96@gmail.com>
2021-11-29 17:59:23 +0300

    Making the API Better: Aliasing

--- src/routes/tours.js
+++ src/routes/tours.js
@@ -4,6 +4,10 @@ const toursController = require('../controllers/tours')
 const router = express.Router()
 
 router
+    .route('/top-5-cheap')
+    .get(toursController.aliasTopTours, toursController.getAllTours)
+
+router
     .route('/')
     .get(toursController.getAllTours)
     .post(toursController.createTour)
