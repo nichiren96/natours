22874f7ccadcffca352a29837a4ee409c66912d4
Roel Tombozafy <rtombozafy96@gmail.com>
2021-12-01 18:40:19 +0300

    Refactoring

--- src/controllers/tours.js
+++ src/controllers/tours.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tour')
+const QueryParser = require('../utils/queryParser')
 
 exports.aliasTopTours = (req, res, next) => {
 
@@ -13,49 +14,10 @@ exports.getAllTours = async (req, res) => {
 
     try {
 
-        console.log(req.query)
-        // BUILD QUERY
-        // 1) filtering
-        const queryObj = {...req.query}
-        const excludedFields = ['page', 'sort', 'limit', 'fields']
-        excludedFields.forEach(el => delete queryObj[el])
-       
-        // 2) Advanced filtering
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-       
-        let query = Tour.find(JSON.parse(queryStr))
-
-        // 3) Sorting
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ')
-            query = query.sort(sortBy)
-        } else {
-            query = query.sort('-createdAt')
-        }
-
-        // 4) Field limiting
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ')
-            query = query.select(fields)
-        } else {
-            query = query.select('-__v')
-        }
-
-        // 5) Pagination
-        const page = req.query.page * 1 || 1
-        const limit = req.query.limit * 1 || 5
-        const skip = (page - 1) * limit
-        // page=2&limit=10, 1-10: page 1, 11-20: page 2, 21-30: page 3
-        query = query.skip(skip).limit(limit)
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments()
-            if (skip >= numTours) throw new Error('This page does not exist')
-        }
-
-        // EXECUTE QUERY
-        const tours = await query
+        const queryParser = new QueryParser(Tour.find(), req.query)
+                                .filter().sort().limitFields().paginate()
+        
+        const tours = await queryParser.query
 
         // SEND RESPONSE
         res.status(200).json({
