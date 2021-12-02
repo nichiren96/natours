04f53f1a1a67e8fbbce744732143431059c183df
Roel Tombozafy <rtombozafy96@gmail.com>
2021-12-02 19:14:01 +0300

    Document Middleware

--- src/models/tour.js
+++ src/models/tour.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -6,6 +7,9 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
         trim: true
+    },
+    slug: {
+        type: String
     },
     duration: {
         type: Number,
@@ -57,6 +61,22 @@ const tourSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 })
 
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, {lower: true})
+    next()
+})
+
+/* tourSchema.pre('save', function(next) {
+    console.log('Will save document...')
+    next()
+})
+
+tourSchema.post('save', function(doc, next) {
+    console.log(doc)
+    next()
+})
+ */
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7
 })
