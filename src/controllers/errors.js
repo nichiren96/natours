4c94c8a1563c0f2a64d416b9fab62d5b8bb1d2cf
Roel Tombozafy <rtombozafy96@gmail.com>
2021-12-03 21:03:37 +0300

    Handling Invalid Database IDs & Duplicate Database Fields

--- src/controllers/errors.js
+++ src/controllers/errors.js
@@ -1,3 +1,16 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate fields value: ${value}. Please use another value`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -21,7 +34,7 @@ const sendErrorProd = (err, res) => {
     // 2) Send generic message
     res.status(500).json({
       status: "error",
-      message: "Something went rong",
+      message: "Something went wrong",
     });
   }
 };
@@ -30,9 +43,19 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
+  console.log(process.env.NODE_ENV);
+
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (err.name === "CastError") {
+      error = handleCastErrorDB(error);
+    }
+
+    if (err.code === 11000) {
+      error = handleDuplicateFieldsDB(err);
+    }
+    sendErrorProd(error, res);
   }
 };
