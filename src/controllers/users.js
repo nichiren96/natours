7974b3c52284b3df88fbc9f5faf8245a6e02b8b6
Roel Tombozafy <rtombozafy96@gmail.com>
2021-12-07 11:37:37 +0300

    Logging in Users

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,16 +1,18 @@
-exports.getAllUsers = (req, res) => {
-  try {
-    res.status(200).json({
-      status: "success",
-      message: "Not yet implemented",
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
 
 exports.getUser = (req, res) => {
   try {
