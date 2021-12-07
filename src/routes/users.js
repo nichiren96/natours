592750b4e22eff637f3261660c91766a465b8904
Roel Tombozafy <rtombozafy96@gmail.com>
2021-12-07 09:46:15 +0300

    Creating New Users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const usersController = require("../controllers/users");
+const authController = require("../controllers/auth");
 
 const router = express.Router();
+
+router.post("/signup", authController.signup);
 
 router
   .route("/")
