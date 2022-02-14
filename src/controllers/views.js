90dd05d83f6d134c5688b435d9e1718028d1ad0b
Roel Tombozafy <rtombozafy96@gmail.com>
2022-02-14 15:27:46 +0300

    Building the Tour Overview - Part 1

--- src/controllers/views.js
+++ src/controllers/views.js
@@ -1,6 +1,11 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render("overview", { title: "All tours" });
-};
+const Tour = require("../models/tour");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  res.status(200).render("overview", { title: "All tours", tours });
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render("tour", { title: "Baobab Tours" });
